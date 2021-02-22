186babd25f9a527e311cf60681ff880d7953891a
lucuxx <49752634+lucuxx@users.noreply.github.com>
2021-02-22 23:28:31 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createApp } from 'vue';
 import 'normalize.css'; // CSS reset的替代方案
 import '@/assets/styles/style.scss'; // 引入全局样式
 
-import App from './app.vue';
+import App from './App.vue';
 import router from './router';
 import store from './store';
 
